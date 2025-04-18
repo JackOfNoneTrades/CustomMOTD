53c2e2ecb416b7910223d83e05b97f7c69cc0e96
jack <jack>
2025-04-18 16:12:58 +0200

    temp fix for tabfaces

--- src/main/java/org/fentanylsolutions/cmotd/util/Util.java
+++ src/main/java/org/fentanylsolutions/cmotd/util/Util.java
@@ -8,7 +8,7 @@ import cpw.mods.fml.common.FMLCommonHandler;
 
 public class Util {
 
-    public static final String fakePlayerUUIDString = "deadbeef-dead-dead-dead-deadbeefdead";
+    public static final String fakePlayerUUIDString = "feadbeef-dead-dead-dead-deadbeefdead";
     public static final UUID fakePlayerUUID = UUID.fromString(fakePlayerUUIDString);
 
     public static boolean isOp(EntityPlayerMP entityPlayerMP) {
